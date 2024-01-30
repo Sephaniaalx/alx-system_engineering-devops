0bed14f06695971d7d63c57473e1b7ce47658ad9
Sephaniaalx <122804994+Sephaniaalx@users.noreply.github.com>
2024-01-30 05:38:58 +0100

    all done

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,18 @@
+# This installs an Nginx server with a Custom Header 'X-Served-By'
+
+exec { 'update':
+    provider => shell,
+    command  => 'sudo apt-get -y update',
+    before   => Exec['install Nginx'],
+}
+
+exec { 'install Nginx':
+    provider => shell,
+    command  => 'sudo apt-get -y install nginx',
+    before   => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+    provider => shell,
+    command  => 'sudo service nginx restart'
+}
