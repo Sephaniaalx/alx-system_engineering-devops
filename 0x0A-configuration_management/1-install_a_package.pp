ce49f0a2322c34ba47b840fabffa03a1a97ff513
Sephaniaalx <122804994+Sephaniaalx@users.noreply.github.com>
2024-02-17 22:15:18 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Instals flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Puppet manifest for killing a process named "killmenow"
-
-exec { 'kill':
-  command => 'pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin']
-}
